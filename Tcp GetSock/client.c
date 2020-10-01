307dcd64441270bf18f6b37ee690752a90247b39
Devang Arora <devangraj.arora@gmail.com>
2020-10-01 12:30:32 +0530

    Update client.c
    changed explanation

--- Tcp GetSock/client.c
+++ Tcp GetSock/client.c
@@ -1,22 +1,18 @@
-/****************** CLIENT CODE ****************/
-
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
 
 int main(){
+  
   int clientSocket;
   char buffer[1024];
   struct sockaddr_in serverAddr;
   socklen_t addr_size;
-
-  /*---- Create the socket. The three arguments are: ----*/
-  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
+  
   clientSocket = socket(PF_INET, SOCK_STREAM, 0);
   
-  /*---- Configure settings of the server address struct ----*/
-  /* Address family = Internet */
+
   serverAddr.sin_family = AF_INET;
   /* Set port number, using htons function to use proper byte order */
   serverAddr.sin_port = htons(7891);
@@ -32,7 +28,6 @@ int main(){
   /*---- Read the message from the server into the buffer ----*/
   recv(clientSocket, buffer, 1024, 0);
 
-  /*---- Print the received message ----*/
   printf("Data received: %s",buffer);   
   
   close(clientSocket);
