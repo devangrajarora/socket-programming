951dc603ac48d0289ed314606b303d65852b8845
Devang Arora <devangraj.arora@gmail.com>
2020-10-01 12:32:31 +0530

    Update client.c

--- Tcp GetSock/client.c
+++ Tcp GetSock/client.c
@@ -1,25 +1,23 @@
-/****************** CLIENT CODE ****************/
-
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
+#include<arpa/inet.h>
+#define PORT 7891
+#define SIZE 1024
 
 int main(){
+  
   int clientSocket;
-  char buffer[1024];
+  char buffer[SIZE];
   struct sockaddr_in serverAddr;
   socklen_t addr_size;
 
-  /*---- Create the socket. The three arguments are: ----*/
-  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
   clientSocket = socket(PF_INET, SOCK_STREAM, 0);
-  
-  /*---- Configure settings of the server address struct ----*/
-  /* Address family = Internet */
+
   serverAddr.sin_family = AF_INET;
   /* Set port number, using htons function to use proper byte order */
-  serverAddr.sin_port = htons(7891);
+  serverAddr.sin_port = htons(PORT);
   /* Set IP address to localhost */
   serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
   /* Set all bits of the padding field to 0 */
@@ -30,7 +28,7 @@ int main(){
   connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
 
   /*---- Read the message from the server into the buffer ----*/
-  recv(clientSocket, buffer, 1024, 0);
+  recv(clientSocket, buffer, SIZE, 0);
 
   /*---- Print the received message ----*/
   printf("Data received: %s",buffer);   
